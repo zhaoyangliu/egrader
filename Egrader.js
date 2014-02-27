c1016ce154c4226d60f4fcf932ee42e2b934f881
zhaoyangliu <zhaoyangliu2013@u.northwestern.edu>
2014-02-27 12:55:36 -0600

    merge all

---
+++ Egrader.js
@@ -0,0 +1,30 @@
+$(function(){
+	$("#comment1").toggle();
+	$("#score_completed").hide();
+	
+	$(".score_input").bind("input",function(){
+		$("#score_completed").hide();
+		var score_input = $(".score_input");
+		var sum_input = $("#sum_input");
+		sum_input.val("0");
+		var sum = 0;
+		for (var i = 0; i < score_input.length; i++) {
+			if (score_input.eq(i).val() != undefined && score_input.eq(i).val() != ""){
+				sum += parseFloat(score_input.eq(i).val());
+			}
+		}
+		var decimal_point = String(sum).indexOf(".");
+		if (decimal_point != -1) 
+			sum_input.val(String(sum).substring(0, decimal_point + 3));
+		else 
+			sum_input.val(String(sum));
+	});
+
+	$(".notes_image").click(function() {
+		$("#comment1").toggle();
+	});
+	
+	$("#score_complete").click(function() {
+		$("#score_completed").show();
+	});
+});
