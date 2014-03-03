ae83bde20f231100effcf2ca8b8f1d4a887cd06b
zhaoyangliu <zhaoyangliu2013@u.northwestern.edu>
2014-03-02 21:43:20 -0600

    ..

--- Egrader.js
+++ Egrader.js
@@ -1,6 +1,8 @@
 $(function(){
 	$("#comment1").toggle();
 	$("#score_completed").hide();
+	window.mark_press = 0;
+	window.hw_img = 0;
 	
 	$(".score_input").bind("input",function(){
 		$("#score_completed").hide();
@@ -27,6 +29,36 @@ $(function(){
 	$("#score_complete").click(function() {
 		$("#score_completed").show();
 	});
+	
+	$("#mark_btn").click(function() {
+		//$("*").css("cursor", "url(mark.gif) 2 2, auto");
+
+		if (window.mark_press == 0) {
+			$("*").css("cursor", "url(mark.gif) 3 3, auto");
+			window.mark_press = 1;
+		}
+		else if (window.mark_press == 1) {
+			$("*").css("cursor", "auto");
+			window.mark_press = 0;
+		}
+		
+	});
+
+	$("#hwImg").click(function() {
+		if (window.mark_press == 1)
+			$("#hwImg").attr("src", "img/hw2_marked.jpg");
+	});
+
+	$(".arrow_img").click(function() {
+		if (window.hw_img == 0) {
+			$("#hwImg").attr("src", "img/hw5.jpg");
+			window.hw_img = 1;
+		}
+		else if (window.hw_img == 1) {
+			$("#hwImg").attr("src", "img/hw2.jpg");
+			window.hw_img = 0;	
+		}
+	});
 });
 
 function zoomIn(){
@@ -38,3 +70,10 @@ function zoomOut(){
     $("#hwImg").css('width', $("#hwImg").width() - $(window).width() * 0.1);
     $("#hwImg").css('height', 'auto');
 }
+
+
+
+
+
+
+
